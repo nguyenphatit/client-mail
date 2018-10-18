9a33f680c7b80a728dc5e0497f6d3c25c05d39e8
Nguyen Phat <nguyenphatit.nl@gmail.com>
2018-10-18 11:03:18 +0700

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,17 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import appReducer from './reducers/index';
-//import thunk from 'redux-thunk';
-//import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
-//const middleware = [thunk];
-// if (process.env.NODE_ENV !== 'production') {
-//     middleware.push(createLogger());
-// }
+const middleware = [thunk];
+if (process.env.NODE_ENV !== 'production') {
+    middleware.push(createLogger());
+}
 
 const store = createStore(
     appReducer,
 //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-//     applyMiddleware(...middleware)
+    applyMiddleware(...middleware)
 );
 
 export default store;
